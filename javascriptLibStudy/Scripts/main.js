2c0beebc4e5c1afe2a93b04f2fc5f40d6a3e41df
Cliff Zhu <v-clzh@microsoft.com>
2016-10-11 14:38:30 +0800

    delete *.suo from git

--- javascriptLibStudy/Scripts/main.js
+++ javascriptLibStudy/Scripts/main.js
@@ -1,4 +1,4 @@
-﻿/// <autosync enabled="true" />
+﻿/// <autosync enabled="true" />//123123
 /// <reference path="../ScriptsNew/require.js" data-main="ScriptsNew/main.js" />
 require.config({
     paths: {
